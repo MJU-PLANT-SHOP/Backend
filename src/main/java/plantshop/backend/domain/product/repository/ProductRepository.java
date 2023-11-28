5b8a0cd4a37bbddc4ebca50e4b153894de6c7fd6
taeyoungro <tyro981009@naver.com>
2023-11-28 14:58:31 +0900

    feat: 상품-api
    1127 feat: 상품-api

---
+++ src/main/java/plantshop/backend/domain/product/repository/ProductRepository.java
@@ -0,0 +1,20 @@
+package plantshop.backend.domain.product.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import plantshop.backend.domain.post.entity.Post;
+import plantshop.backend.domain.product.dto.response.GetProductListResponseDto;
+import plantshop.backend.domain.product.entity.Category;
+import plantshop.backend.domain.product.entity.Product;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("SELECT p FROM Product p WHERE p.category = :category")
+    List<Product> findProductsByCategory(@Param("category") Category category);
+
+}
